a646baf030ae17d134cd496b415f07a0e6c6710e
Grigory Petrov <grigory.v.p@gmail.com>
2015-02-24 14:27:46 +0300

    Added nginx local debug config fenerator.

--- generators/nginx-local/index.js
+++ generators/nginx-local/index.js
@@ -7,6 +7,14 @@ module.exports = generators.Base.extend({
       this.templatePath('nginx.conf.tpl'),
       this.destinationPath('conf', 'nginx.conf')
     );
+    this.fs.copyTpl(
+      this.templatePath('mime.types.tpl'),
+      this.destinationPath('conf', 'mime.types')
+    );
+    this.fs.copyTpl(
+      this.templatePath('fastcgi_params.tpl'),
+      this.destinationPath('conf', 'fastcgi_params')
+    );
   }
 });
 
